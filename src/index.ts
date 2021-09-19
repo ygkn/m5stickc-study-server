82be70a5d0ddbd7b474f9d86c65ea1a1a2dbf17b
ygkn <2000ygkn0713@gmail.com>
2021-09-19 20:49:00 +0900

    Hello with Fastify

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello!");
+import fastify from 'fastify';
+
+const server = fastify();
+
+server.get('/', async (request, reply) => {
+  return 'Hello!\n';
+});
+
+server.listen(8080, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log(`Server listening at ${address}`);
+});
